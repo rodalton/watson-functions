d386723763d17febf9628a6fe688455bc5c0813b
Ronan Dalton <daltonro@ie.ibm.com>
2018-10-11 11:03:30 +0100

    IAM API Key

--- discovery.js
+++ discovery.js
@@ -1,3 +1,18 @@
+/**
+  *
+  * @param {object} params
+  * @param {string} params.iam_apikey
+  * @param {string} params.url
+  * @param {string} params.username
+  * @param {string} params.password
+  * @param {string} params.environment_id
+  * @param {string} params.collection_id
+  * @param {string} params.input
+  *
+  * @return {object}
+  *
+  */
+
 const assert = require('assert');
 const DiscoveryV1 = require('watson-developer-cloud/discovery/v1');
 
@@ -6,11 +21,20 @@ function main(params) {
 
       let discovery;
 
-      discovery = new DiscoveryV1({
-      version: '2018-08-01',
-      username: params.username,
-      password: params.password,
-      url: params.url
+      if (iam_apikey){
+        discovery = new DiscoveryV1({
+          iam_apikey: params.iam_apikey,
+          url: params.url
+          version: '2018-08-01'
+        });
+      }
+      else {
+        discovery = new DiscoveryV1({
+        username: params.username,
+        password: params.password,
+        url: params.url,
+        version: '2018-08-01'
+      }
     });
 
       discovery.query({
